d7b92eb15b638a3e2619edcd01d421fe278d5bf9
cilesio-bsj <cilesio.jr@ufu.br>
2022-05-31 11:09:18 -0300

    Definindo variáveis de estado e rotina para piscar LED

--- src/lab-03/main.c
+++ src/lab-03/main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 #define STM32_RCC_BASE 				0x40023800
 #define STM32_GPIOC_BASE 			0x48000800U
@@ -8,11 +9,15 @@
 #define STM32_GPIO_OTYPER_OFFSET 	0x0004 
 #define STM32_GPIO_PUPDR_OFFSET 	0x000c
 
+#define STM32_GPIO_BSRR_OFFSET 		0x0018 
+
 #define STM32_RCC_AHB1ENR			(STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
 
 #define STM32_GPIOC_MODER 			(STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
 #define STM32_GPIOC_OTYPER 			(STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
 #define STM32_GPIOC_PUPDR 			(STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOC_BSRR 			(STM32_GPIOC_BASE + STM32_GPIO_BSRR_OFFSET)
+
 
 #define RCC_AHB1ENR_GPIOCEN 		(1 << 2)
 
@@ -34,11 +39,17 @@
 #define GPIO_PUPDR13_SHIFT 	(26)
 #define GPIO_PUPDR13_MASK 	(3 << GPIO_PUPDR13_SHIFT)
 
+#define GPIO_BSRR13_SET 	(0) //Liga
+#define GPIO_BSRR13_RESET 	(1) //Desliga
+
+static char fw_version[] = {'V', '1', '.', '0'};
+static uint32_t led_status;
+static int LED_DELAY = 50000;
+
 int main(int argc, char *argv[])
 {
-
 	uint32_t reg;
-	
+
 	// Ponteiros para registradores
 	uint32_t *pRCC_AHB1ENR = (uint32_t *)STM32_RCC_AHB1ENR;
 	uint32_t *pGPIOC_MODER = (uint32_t *)STM32_GPIOC_MODER;
@@ -46,12 +57,39 @@ int main(int argc, char *argv[])
 	uint32_t *pGPIOC_PUPDR = (uint32_t *)STM32_GPIOC_PUPDR;
 	uint32_t *pGPIOC_BSRR = (uint32_t *)STM32_GPIOC_BSRR;
 
+	#define GPIO_BSRR_SET(n) (1 << (n))
+	#define GPIO_BSRR_RST(n) (1 << (n + 16))
+
 	// Habilita clock GPIOC
 	reg = *pRCC_AHB1ENR;
 	reg |= RCC_AHB1ENR_GPIOCEN;
 	*pRCC_AHB1ENR = reg;
 
-	while(1){};
+	reg = *pGPIOC_MODER;
+	reg &= ~(GPIO_MODER13_MASK);
+	reg |= (GPIO_MODER_OUTPUT << GPIO_MODER13_SHIFT);
+	*pGPIOC_MODER = reg;
+	reg = *pGPIOC_OTYPER;
+	reg &= ~(GPIO_OT13_MASK);
+	reg |= (GPIO_OTYPER_PP << GPIO_OT13_SHIFT);
+	*pGPIOC_OTYPER = reg;
+	reg = *pGPIOC_PUPDR;
+	reg &= ~(GPIO_PUPDR13_MASK);
+	reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR13_SHIFT);
+	*pGPIOC_PUPDR = reg;
+
+
+	while(1)
+	{
+	
+	*pGPIOC_BSRR = GPIO_BSRR13_SET;
+	led_status = 0;
+	for (uint32_t i = 0; i < LED_DELAY; i++);
+	*pGPIOC_BSRR = GPIO_BSRR13_RESET;
+	led_status = 1;
+	for (uint32_t i = 0; i < LED_DELAY; i++);
+
+	};
 
 	return EXIT_SUCCESS;
 }
