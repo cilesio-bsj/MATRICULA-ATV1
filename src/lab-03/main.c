40e9cb3934ae7f48e437acdf512a8065271f03dc
cilesio-bsj <cilesio.jr@ufu.br>
2022-05-31 10:28:58 -0300

    Habilitar o clock

--- src/lab-03/main.c
+++ src/lab-03/main.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 
+#define STM32_RCC_BASE 				0x40023800
+#define STM32_RCC_AHB1ENR_OFFSET	0x0030
+#define STM32_RCC_AHB1ENR			(STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
+#define RCC_AHB1ENR_GPIOCEN 		(1 << 2)
+
 int main(int argc, char *argv[])
 {
 	while(1){};
